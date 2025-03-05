d61967a91a4653e55ed5099b6c2c564cb22cdc8e
AJ Wells <wells19d@gmail.com>
2025-03-05 10:06:41 -0600

    updated some packages and starting setup for navigation rebuild

---
+++ src/components/IconListRouter.jsx
@@ -0,0 +1,91 @@
+//*IconListRouter.jsx
+import ADIcons from 'react-native-vector-icons/AntDesign';
+import FAIcons from 'react-native-vector-icons/FontAwesome';
+import FA5Icons from 'react-native-vector-icons/FontAwesome5';
+import MCIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import MIIcons from 'react-native-vector-icons/MaterialIcons';
+import IIcons from 'react-native-vector-icons/Ionicons';
+import EIcons from 'react-native-vector-icons/Entypo';
+import FIcons from 'react-native-vector-icons/Fontisto';
+import FEIcons from 'react-native-vector-icons/Feather';
+import SIIcons from 'react-native-vector-icons/SimpleLineIcons';
+import MIcons from 'react-native-vector-icons/MaterialIcons';
+import ZOIcons from 'react-native-vector-icons/Zocial';
+import {Keyboard} from 'react-native';
+
+export const Icons = {
+  Account: props => (
+    <MCIcons name="account-circle" size={20} color="#000" {...props} />
+  ),
+  AddList: props => (
+    <MCIcons name="playlist-plus" size={20} color="#000" {...props} />
+  ),
+  Back: props => (
+    <MIIcons name="arrow-back-ios" size={20} color="#000" {...props} />
+  ),
+  Barcode: props => (
+    <FAIcons name="barcode" size={20} color="#000" {...props} />
+  ),
+  ChevronDown: props => (
+    <FEIcons name="chevron-down" size={20} color="#000" {...props} />
+  ),
+  ChevronUp: props => (
+    <FEIcons name="chevron-up" size={20} color="#000" {...props} />
+  ),
+  CirclePlus: props => (
+    <ADIcons name="pluscircleo" size={20} color="#000" {...props} />
+  ),
+  Close: props => <MCIcons name="close" size={20} color="#000" {...props} />,
+  Cupboards: props => (
+    <MCIcons name="wardrobe" size={20} color="#000" {...props} />
+  ),
+  Edit: props => <MIIcons name="edit-note" size={20} color="#000" {...props} />,
+  Favorite: props => <MCIcons name="star" size={20} color="#000" {...props} />,
+  Filter: props => (
+    <IIcons name="filter-outline" size={20} color="#000" {...props} />
+  ),
+  Forward: props => (
+    <MIIcons name="arrow-forward-ios" size={20} color="#000" {...props} />
+  ),
+  Home: props => <MCIcons name="home" size={20} color="#000" {...props} />,
+  Help: props => (
+    <MCIcons name="help-circle" size={20} color="#000" {...props} />
+  ),
+  Keyboard: props => (
+    <MCIcons name="keyboard-variant" size={20} color="#000" {...props} />
+  ),
+  Menu: props => <FEIcons name="menu" size={20} color="#000" {...props} />,
+  MenuList: props => <MCIcons name="menu" size={20} color="#000" {...props} />,
+  LightOn: props => (
+    <MIIcons name="flashlight-on" size={20} color="#000" {...props} />
+  ),
+  LightOff: props => (
+    <MIIcons name="flashlight-off" size={20} color="#000" {...props} />
+  ),
+  Logout: props => <MIIcons name="logout" size={20} color="#000" {...props} />,
+  Merge: props => (
+    <MCIcons name="set-merge" size={20} color="#000" {...props} />
+  ),
+  Plus: props => <MCIcons name="plus" size={20} color="#000" {...props} />,
+  Profile: props => (
+    <ADIcons name="profile" size={20} color="#000" {...props} />
+  ),
+  Recipe: props => (
+    <MCIcons name="treasure-chest" size={20} color="#000" {...props} />
+  ),
+  Save: props => (
+    <MCIcons name="content-save" size={20} color="#000" {...props} />
+  ),
+  Search: props => <MIIcons name="search" size={20} color="#000" {...props} />,
+  Settings: props => (
+    <FA5Icons name="wrench" size={20} color="#000" {...props} />
+  ),
+  Shopping: props => (
+    <FAIcons name="shopping-cart" size={20} color="#000" {...props} />
+  ),
+  Split: props => (
+    <MCIcons name="set-split" size={20} color="#000" {...props} />
+  ),
+  Wave: props => <MCIcons name="wave" size={20} color="#000" {...props} />,
+  Waves: props => <MCIcons name="waves" size={20} color="#000" {...props} />,
+};
---
+++ src/utilities/deviceUtils.js
@@ -0,0 +1,61 @@
+// deviceUtils.js
+export const hbDevices = [
+  'iPhone SE',
+  'iPad Air 3rd Gen (WiFi)',
+  'iPad Air 3rd Gen (Cellular)',
+  // Add other home-button models here as needed
+];
+
+export const isHBDevice = model => hbDevices.includes(model);
+
+export const isAndroidPhone = device =>
+  device?.system?.os === 'Android' && device?.system?.device === 'Handset';
+
+export const isAndroidTablet = device =>
+  device?.system?.os === 'Android' && device?.system?.device === 'Tablet';
+
+export const isiOSPhone = device =>
+  device?.system?.os === 'iOS' && device?.system?.device === 'Handset';
+
+export const isiOSTablet = device =>
+  device?.system?.os === 'iOS' && device?.system?.device === 'Tablet';
+
+export const getNavMenuHeight = device => {
+  const {deviceSize, model, os} = device?.system || {};
+  const isHomeButton = isHBDevice(model);
+
+  let baseHeight = 90; // Fallback for unknown devices (just to be safe)
+
+  switch (deviceSize) {
+    case 'xLarge':
+      baseHeight = 110;
+      break;
+    case 'large':
+      baseHeight = 105;
+      break;
+    case 'medium':
+      baseHeight = 100;
+      break;
+    case 'small':
+      baseHeight = 95;
+      break;
+    case 'xSmall':
+      baseHeight = 90;
+      break;
+    default:
+      baseHeight = 90; // Unknown devices default to xSmall
+      break;
+  }
+
+  // Special adjustment for iOS devices
+  if (os === 'iOS') {
+    baseHeight -= 5;
+  }
+
+  // Special adjustment for home button devices
+  if (isHomeButton) {
+    baseHeight -= 10;
+  }
+
+  return baseHeight;
+};
